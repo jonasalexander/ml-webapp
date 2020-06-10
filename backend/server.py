644cec0b06ceae565cc9e7a3a08347027867f59e
Jonas Nelle <jonas.a.nelle@gmail.com>
2020-06-10 22:51:32 +0200

    cleanup and separate create, predict endpoints

--- backend/server.py
+++ backend/server.py
@@ -6,12 +6,13 @@ import os
 
 ID_LEN = 30
 DATA_PATH = Path("data/")
+FRONTEND_URL = "http://localhost:3000"
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST"])
-def hello_world():
+@app.route("/create", methods=["POST"])
+def create_model():
     model_id = "".join(random.choices(string.ascii_uppercase + string.digits, k=ID_LEN))
 
     if not DATA_PATH.exists():
@@ -22,5 +23,11 @@ def hello_world():
     f.save(DATA_PATH / model_data_path)
 
     response = Response(model_id, mimetype="text/text")
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
+    return response
+
+@app.route("/<model_id>")
+def predict(model_id):
+    response = Response(model_id, mimetype="text/text")
+    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
     return response
