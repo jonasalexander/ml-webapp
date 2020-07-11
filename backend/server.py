301545dd257dafc37c9c45faf0e6654cff539660
Jonas Nelle <jonas.a.nelle@gmail.com>
2020-07-11 19:58:37 +0200

    bugfix stop early if file shorter than NUM_PREVIEW_ROWS

--- backend/server.py
+++ backend/server.py
@@ -8,36 +8,53 @@ import json
 ID_LEN = 30
 DATA_PATH = Path("data/")
 FRONTEND_URL = "http://localhost:3000"
-NUM_PREVIEW_ROWS = 5
+NUM_PREVIEW_ROWS = 10
 
 app = Flask(__name__)
 
 
 @app.route("/create", methods=["POST"])
 def create_model():
-    model_id = "".join(random.choices(string.ascii_uppercase + string.digits, k=ID_LEN))
 
     if not DATA_PATH.exists():
         os.mkdir(DATA_PATH)
 
     f = request.files["file"]
-    model_data_path = model_id + ".csv"
-    full_path = DATA_PATH / model_data_path
+
+    while True:
+        model_id = "".join(
+            random.choices(string.ascii_uppercase + string.digits, k=ID_LEN)
+        )
+        model_data_path = model_id + ".csv"
+        full_path = DATA_PATH / model_data_path
+        if not full_path.exists():
+            break
+
     f.save(full_path)
 
-    response_data = {"model_id": model_id}
+    model_name = request.form["modelName"]
+    # TODO: Add model_name to separate metadata lookup store for name and id,
+    # time created, etc.
+    # (and eventually username, etc)
+
+    response_data = {"model_id": model_id, "model_name": model_name, "csv_data": []}
     with open(full_path, "r") as f:
-        response_data["csv_data"] = [f.readline().split(",") for _ in range(NUM_PREVIEW_ROWS)]
+        for i, row in enumerate(f):
+            if i > NUM_PREVIEW_ROWS:
+                break
+            response_data["csv_data"].append(row.split(","))
 
     response = Response(json.dumps(response_data), mimetype="application/json")
     response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
     return response
+
 
 @app.route("/<model_id>")
 def predict(model_id):
     response = Response(model_id, mimetype="text/text")
     response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
     return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
