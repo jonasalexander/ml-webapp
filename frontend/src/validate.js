d206f197e7a13c28609b78a2cc16992dcaae2cfc
Jonas Nelle <jonas.a.nelle@gmail.com>
2020-07-11 20:00:58 +0200

    show preview data in blueprint HTMLTable

--- frontend/src/validate.js
+++ frontend/src/validate.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import './validate.css'
+import { HTMLTable } from "@blueprintjs/core";
 
 export class ValidateApp extends React.Component {
-    // 
+    renderTableRow = (tableRow, rowIndex) => {
+        if (rowIndex === 0) {
+            return (
+                <tr key={rowIndex}>
+                    {tableRow.map((row, columnIndex) => (<th key={columnIndex}>{row}</th>))}
+                </tr>
+            );
+        }
+        return (
+            <tr key={rowIndex}>
+                {tableRow.map((row, columnIndex) => (<td key={columnIndex}>{row}</td>))}
+            </tr>
+        );
+    }
+
+    renderTable = (csvPreview) => {
+        console.log(csvPreview);
+
+        return (
+            <div>
+                <div>
+                    Please check the data you submitted and give the model context on how
+                    to interpret the data.
+                </div>
+                <HTMLTable
+                    className="validation-table"
+                    interactive={true}
+                    striped={true}
+                >
+                    <thead>
+                        {csvPreview.slice(0, 1).map((row, rowIndex) => this.renderTableRow(row, rowIndex))}
+                    </thead>
+                    <tbody>
+                        {csvPreview.slice(1).map((row, rowIndex) => this.renderTableRow(row, rowIndex+1))}
+                    </tbody>
+                </HTMLTable>
+            </div>
+        );
+    }
 
     render() {
-        const { modelId, csvPreview } = this.props;
-        return (<h2>Validate</h2>)
+        const { csvPreview } = this.props;
+
+        return (
+            <div>
+                <h2>Validate</h2>
+                {this.renderTable(csvPreview)}
+            </div>
+        );
     }
 }
